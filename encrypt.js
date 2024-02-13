7e65997d2a05eae31516e087f0992bf0a106f89c
Helio Nunes <helioaugustofn@protonmail.com>
2024-02-12 21:31:04 -0400

    Adicionado esqueleto do projeto e funções para criptografar e decodificar

---
+++ encrypt.js
@@ -0,0 +1,10 @@
+function criptografar() {
+  let texto = document.getElementById("botao-criptografar");
+  return texto;
+}
+
+function decodificar() {
+  let textoADecodificar = criptografar();
+  let textoDecodificado = "";
+  return textoDecodificado;
+}
