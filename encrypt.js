efd6be7f9520f0a0b1540cdb69c3e4bd7fb7e23e
Helio Nunes <helioaugustofn@protonmail.com>
2024-02-14 16:32:19 -0400

    Mudança no nome de atributos no html e conclusão da versão inicial da função de criptografia

--- encrypt.js
+++ encrypt.js
@@ -1,10 +1,37 @@
 function criptografar() {
-  let texto = document.getElementById("botao-criptografar");
-  return texto;
+  let texto = document.getElementById("campo-entrada").value;
+  let arrayTexto = texto.split("");
+  let textoCodificado = "";
+
+  for (let i = 0; i < arrayTexto.length; i++) {
+    const element = arrayTexto[i];
+    if (element === "a") {
+      arrayTexto[i] += "i";
+    } else if (element === "e") {
+      arrayTexto[i] += "nter";
+    } else if (element === "i") {
+      arrayTexto[i] += "mes";
+    } else if (element === "o") {
+      arrayTexto[i] += "ber";
+    } else if (element === "u") {
+      arrayTexto[i] += "fat";
+    }
+  }
+
+  for (const str of arrayTexto) {
+    textoCodificado += str;
+  }
+  exibirTexto(textoCodificado, "campo-saida");
 }
 
-function decodificar() {
-  let textoADecodificar = criptografar();
+function decodificar(textoCodificado) {
+  // Lógica para descriptografar aqui
+  let arrayTexto = textoCodificado.split("");
   let textoDecodificado = "";
-  return textoDecodificado;
+  exibirTexto(textoDecodificado, "campo-saida");
+}
+
+function exibirTexto(texto, idElemento) {
+  let campo = document.getElementById(idElemento);
+  campo.innerHTML = texto;
 }
